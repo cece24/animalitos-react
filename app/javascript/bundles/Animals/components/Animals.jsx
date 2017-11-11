c065ee8b8bd92ae3f4cf32d8b304d37b76b26da9
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-11 17:01:59 -0500

    Set up Animals react component

---
+++ app/javascript/bundles/Animals/components/Animals.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default class Animals extends React.Component {
+  static propTypes = {
+    animals: PropTypes.array.isRequired, // this is passed from the Rails view
+  };
+
+  /**
+   * @param props - Comes from your rails view.
+   */
+  constructor(props) {
+    super(props);
+
+    // How to set initial state in ES6 class syntax
+    // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
+    this.state = { };
+  }
+
+  render() {
+    return (
+      <div>
+        Inside react
+      </div>
+    );
+  }
+}
